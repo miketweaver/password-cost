6ecdc53c2b73eb8510614bfadd1b4dd14063dec7
Mike T. Weaver <miketweaver@Mikes-MacBook-Pro.local>
2018-05-13 14:49:24 -0600

    update appJar, python3, and formatting

--- pass-cost.py
+++ pass-cost.py
@@ -1,45 +1,49 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from appJar import gui
 import getpass
 import argparse
 
 # Flags/Arguments
-parser = argparse.ArgumentParser(description='Process some integers.')
-parser.add_argument('--show-pass', action="store_true",help='prints the typed password')
+parser = argparse.ArgumentParser(description='Process some passwords.', allow_abbrev=True)
+parser.add_argument('--show-pass', action="store_true", help='prints the typed password')
 parser.add_argument('--hash', help='set the hash type. SHA1, MD5, or BCRYPT  (default: %(default)s)', type=str, default="SHA1")
-parser.add_argument('--gui', action="store_true",help='use GUI interface')
+parser.add_argument('--gui', action='store_true', help='use GUI interface')
 args = parser.parse_args()
 
-### Variables ###
+# ## Variables ###
 google = .7
 amazon = .9
 
-### Speed variables ###
-#SHA1 in MH/s
-gtx1070SHA1		= 6401.6
-gtx970SHA1		= 3830.8
-dadSHA1			= 1856.4
-cloudSHA1		= 1979.0375
+# ## Speed variables ###
+# SHA1 in MH/s
 
-#MD5 in MH/s
-gtx1070MD5		= 17907.6
-gtx970MD5		= 9526.5
-dadMD5			= 1
-cloudMD5		= 4580.40625
+gtx1070SHA1 = 6401.6
+gtx970SHA1 = 3830.8
+dadSHA1 = 1856.4
+cloudSHA1 = 1979.0375
 
-#BCRYPT in MH/s
-gtx1070BCRYPT		= .009676
-gtx970BCRYPT		= .006301
-dadBCRYPT		= 1
-cloudBCRYPT		= .002121
+# MD5 in MH/s
+gtx1070MD5 = 17907.6
+gtx970MD5 = 9526.5
+dadMD5 = 1
+cloudMD5 = 4580.40625
 
-units = 1000000 #MH/s
+# BCRYPT in MH/s
+gtx1070BCRYPT = .009676
+gtx970BCRYPT = .006301
+dadBCRYPT = 1
+cloudBCRYPT = .002121
+
+# MH/s
+units = 1000000
+
 
 # Humanize Function
 def humanize_time(secs):
     mins, secs = divmod(secs, 60)
     hours, mins = divmod(mins, 60)
     return '%02d hr %02d min %02d sec' % (hours, mins, secs)
+
 
 def get_charset(string):
 	count = 0
@@ -89,7 +93,8 @@ def get_speed(device,type):
 		}
 	return switcher.get(type, 0)
 
-################ GUI ################
+
+# ############### GUI ################
 if args.gui:
 	# function called by pressing the buttons
 	def press(btn):
@@ -102,25 +107,23 @@ if args.gui:
 			hashtype = app.getOptionBox("HashType")
 			length = len(var)
 			charcount = get_charset(var)
-			hashes = get_hashcount(charcount,length)
-			cloudtime= get_time2crack(hashes,get_speed("CLOUD",hashtype))
+			hashes = get_hashcount(charcount, length)
+			cloudtime=get_time2crack(hashes, get_speed("CLOUD", hashtype))
 
 			text =	"# of Chars: " + str(length)
 			text +=	"\n# of Charset: " + str(charcount)
 			text +=	"\n# of Hashes: " + str(hashes)
 			text +=	"\n# of Hashes: " + str(hashes)
 			text +=	"\nHash Type: " + hashtype
-			text +=	"\nTime to solve (dad): " + humanize_time(get_time2crack(hashes,get_speed("DAD",hashtype)))
-			text +=	"\nTime to solve (gtx 970): " + humanize_time(get_time2crack(hashes,get_speed("GTX970",hashtype)))
-			text +=	"\nTime to solve (gtx 1070): " + humanize_time(get_time2crack(hashes,get_speed("GTX1070",hashtype)))
+			text +=	"\nTime to solve (dad): " + humanize_time(get_time2crack(hashes, get_speed("DAD",hashtype)))
+			text +=	"\nTime to solve (gtx 970): " + humanize_time(get_time2crack(hashes, get_speed("GTX970",hashtype)))
+			text +=	"\nTime to solve (gtx 1070): " + humanize_time(get_time2crack(hashes, get_speed("GTX1070",hashtype)))
 			text +=	"\nCost, Amazon: ${:,.2f}".format(get_cost(cloudtime,amazon))
 			text += "\nCost, Google: ${:,.2f}".format(get_cost(cloudtime,google))
 			app.clearTextArea("Results")
 			app.setTextArea("Results", text)
 
-	app = gui()
-	app=gui("PassCalc", "400x325")
-
+	app = gui("PassCalc", "400x325", handleArgs=False)
 	app.addLabel("title", "Calculate your password", 0, 0, 2)  # Row 0,Column 0,Span 2
 	app.addLabel("by", "by Mike Weaver", 2, 0, 2)  # Row 0,Column 0,Span 2
 	app.addLabel("pass", "Password:", 3, 0)              # Row 2,Column 0
@@ -141,19 +144,19 @@ else:
 	var = getpass.getpass()
 
 	if args.show_pass:
-	 print "Your password is:", var 
+	 print("Your password is: " + var)
 
 	length = len(var)
 	charcount = get_charset(var)
-	hashes = get_hashcount(charcount,length)
-	cloudtime= get_time2crack(hashes,get_speed("CLOUD",args.hash))
+	hashes = get_hashcount(charcount, length)
+	cloudtime= get_time2crack(hashes, get_speed("CLOUD", args.hash))
 
-	print "# of Chars:", length
-	print "# of Charset:", charcount
-	print "# of Hashes:", hashes
-	print "Hash Type:", args.hash
-	print "Time to solve (dad):", humanize_time(get_time2crack(hashes,get_speed("DAD",args.hash)))
-	print "Time to solve (gtx 970):", humanize_time(get_time2crack(hashes,get_speed("GTX970",args.hash)))
-	print "Time to solve (gtx 1070):", humanize_time(get_time2crack(hashes,get_speed("GTX1070",args.hash)))
-	print "Cost, Amazon: ${:,.2f}".format(get_cost(cloudtime,amazon))
-	print "Cost, Google: ${:,.2f}".format(get_cost(cloudtime,google))
+	print("# of Chars: " + str(length))
+	print("# of Charset: " + str(charcount))
+	print("# of Hashes: " + str(hashes))
+	print("Hash Type: " + args.hash)
+	print("Time to solve (dad):", humanize_time(get_time2crack(hashes, get_speed("DAD", args.hash))))
+	print("Time to solve (gtx 970):", humanize_time(get_time2crack(hashes, get_speed("GTX970", args.hash))))
+	print("Time to solve (gtx 1070):", humanize_time(get_time2crack(hashes, get_speed("GTX1070", args.hash))))
+	print("Cost, Amazon: ${:,.2f}".format(get_cost(cloudtime, amazon)))
+	print("Cost, Google: ${:,.2f}".format(get_cost(cloudtime, google)))
